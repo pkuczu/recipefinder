91c6d6b1d1e9c39604a0108f5bec5ac424a23042
Sarah Dowden <111834004+sdowdenillini@users.noreply.github.com>
2023-10-16 20:37:18 +0000

    Updated function to include all pages of recipes

--- webscraping_folder/webbot.py
+++ webscraping_folder/webbot.py
@@ -4,22 +4,8 @@ import sqlite3
 
 conn = sqlite3.connect('recipes.sql')
 
-# request = requests.get("https://www.allrecipes.com/search?q=chicken") # Example with chicken
-# if request.status_code != 200:
-#     print("Error in retrieving the webpage.")
-#     exit()
-# page_html = BeautifulSoup(request.content, 'html5lib')
-
-# links = page_html.find_all("a") # Find all elements with the tag <a>
-
-# f = open("test.txt", "a")
-# for link in links:
-#     if (link.get("href").find("https://www.allrecipes.com/recipe") != -1 and link.get("href").find("https://www.allrecipes.com/recipe/201") == -1 and link.get("href").find("chicken") != -1):
-#         f.write(link.get("href"))
-#         f.write('\n')
-# f.close()
-
 def getRecipeAllRecipes(ingredient):
+    # Get first page 
     url = "https://www.allrecipes.com/search?q=" + ingredient
     request = requests.get(url)
     if request.status_code != 200:
@@ -31,10 +17,35 @@ def getRecipeAllRecipes(ingredient):
 
     f = open("test.txt", "a")
     for link in links:
-        if (link.get("href").find("https://www.allrecipes.com/recipe") != -1 and link.get("href").find("https://www.allrecipes.com/recipe/201") == -1 and link.get("href").find(ingredient) != -1):
+        if (link.get("href").find("https://www.allrecipes.com/recipe") != -1 and link.get("href").find("https://www.allrecipes.com/recipes/201") == -1 and link.get("href").find(ingredient) != -1):
             f.write(link.get("href"))
             f.write('\n')
     f.close()
+
+
+    # Get all the pages after the first 
+
+    num_repeat = len(page_html.find_all("a", class_="button--outlined-little-round type--rabbit-bold")) + 1
+    print(num_repeat)
+
+    for i in range(1, num_repeat):
+        x = i * 24
+        url = "https://www.allrecipes.com/search?" + ingredient + "=" + ingredient + "&" + "offset=" + str(x) + "&q=" + ingredient
+        request = requests.get(url)
+        if request.status_code != 200:
+            print("Error in retrieving the webpage.")
+            exit()
+        page_html = BeautifulSoup(request.content, 'html5lib')
+
+        links = page_html.find_all("a") # Find all elements with the tag <a>
+
+        f = open("test.txt", "a")
+        for link in links:
+            if (link.get("href").find("https://www.allrecipes.com/recipe") != -1 and link.get("href").find("https://www.allrecipes.com/recipes/201") == -1 and link.get("href").find(ingredient) != -1):
+                f.write(link.get("href"))
+                f.write('\n')
+        f.close()
+
 
 getRecipeAllRecipes('chicken')
 getRecipeAllRecipes('turkey')
