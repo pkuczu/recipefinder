4faf377421b2b23e6b9a1cd4094fe2bc71415968
Sarah Dowden <111834004+sdowdenillini@users.noreply.github.com>
2023-10-17 20:11:50 +0000

    added ingredients to allRecipes

--- webscraping_folder/allRecipes.py
+++ webscraping_folder/allRecipes.py
@@ -4,6 +4,8 @@ import sqlite3
 
 conn = sqlite3.connect('recipes.sql')
 
+# current branch name: ingredientUpdate
+
 def getRecipeAllRecipes(ingredient):
     # Get first page 
     url = "https://www.allrecipes.com/search?q=" + ingredient
@@ -13,22 +15,9 @@ def getRecipeAllRecipes(ingredient):
         exit()
     page_html = BeautifulSoup(request.content, 'html5lib')
 
-    links = page_html.find_all("a") # Find all elements with the tag <a>
-
-    f = open("test.txt", "a")
-    for link in links:
-        if (link.get("href").find("https://www.allrecipes.com/recipe") != -1 and link.get("href").find("https://www.allrecipes.com/recipes/201") == -1 and link.get("href").find(ingredient) != -1):
-            f.write(link.get("href"))
-            f.write('\n')
-    f.close()
-
-
-    # Get all the pages after the first 
-
     num_repeat = len(page_html.find_all("a", class_="button--outlined-little-round type--rabbit-bold")) + 1
-    print(num_repeat)
 
-    for i in range(1, num_repeat):
+    for i in range(num_repeat):
         x = i * 24
         url = "https://www.allrecipes.com/search?" + ingredient + "=" + ingredient + "&" + "offset=" + str(x) + "&q=" + ingredient
         request = requests.get(url)
@@ -44,8 +33,20 @@ def getRecipeAllRecipes(ingredient):
             if (link.get("href").find("https://www.allrecipes.com/recipe") != -1 and link.get("href").find("https://www.allrecipes.com/recipes/201") == -1 and link.get("href").find(ingredient) != -1):
                 f.write(link.get("href"))
                 f.write('\n')
+                getIngredientsAllRecipes(link.get("href"))
         f.close()
 
+def getIngredientsAllRecipes(url):
+    request = requests.get(url)
+    if request.status_code != 200:
+        print("Error in retrieving the webpage.")
+        exit()
+    page_html = BeautifulSoup(request.content, 'html5lib')
 
-getRecipeAllRecipes('chicken')
-getRecipeAllRecipes('turkey')
+    ingredients = page_html.find_all(class_="mntl-structured-ingredients__list-item")
+    f2 = open("test2.txt", "a")
+    for i in ingredients:
+        ingredient = str(i.getText())
+        f2.write(ingredient)
+    f2.write('\n')
+    f2.close
