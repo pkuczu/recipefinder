de433c421069b5689da2822ee27db5db571aa9bb
Sarah Dowden <111834004+sdowdenillini@users.noreply.github.com>
2023-10-30 19:27:55 +0000

    Created integration for adding to db in python, but I don't know if it works

---
+++ webscraping_folder/simplyRecipes.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup
+import requests
+import sqlite3
+
+conn = sqlite3.connect('recipes.sql')
+
+# current branch name: simplyRecipes
+
+def getRecipeSimplyRecipes(ingredient):
+    # Get first page 
+    url = "https://www.simplyrecipes.com/search?q=" + ingredient
+    request = requests.get(url)
+    if request.status_code != 200:
+        print("Error in retrieving the webpage.")
+        exit()
+    page_html = BeautifulSoup(request.content, 'html5lib')
+
+    num_repeat = len(page_html.find_all("a", class_="pagination__item")) + 1
+
+    for i in range(num_repeat):
+        x = i * 24
+        url = "https://www.simplyrecipes.com/search?q=" + ingredient + "&offset=" + str(x)
+        if request.status_code != 200:
+            print("Error in retrieving the webpage.")
+            exit()
+        page_html = BeautifulSoup(request.content, 'html5lib')
+
+        links = page_html.find_all(class_="comp card") # Find all elements with the tag <a>
+
+        f = open("test3.txt", "a")
+        for link in links:
+            if (link.get("href").find("https://www.simplyrecipes.com/") != -1 and link.get("href").find(ingredient) != -1):
+                f.write(str(link.get("href")))
+                f.write('\n')
+            #getIngredientsAllRecipes(link.get("href"))
+        f.close()
+
+# def getIngredientsSimplyRecipes(url):
+#     request = requests.get(url)
+#     if request.status_code != 200:
+#         print("Error in retrieving the webpage.")
+#         exit()
+#     page_html = BeautifulSoup(request.content, 'html5lib')
+
+#     ingredients = page_html.find_all(class_="mntl-structured-ingredients__list-item")
+#     f2 = open("test2.txt", "a")
+#     for i in ingredients:
+#         ingredient = str(i.getText())
+#         f2.write(ingredient)
+#     f2.write('\n')
+#     f2.close
+
+getRecipeSimplyRecipes('chicken')
---
+++ webscraping_folder/sql_python_connection.py
@@ -0,0 +1,27 @@
+# branch name: sql_creation_and_push
+
+import sqlite3
+from sqlite3 import Error
+ 
+conn = sqlite3.connect('recipes_test.db')
+cursor = conn.cursor()
+cursor.execute("DROP TABLE IF EXISTS test")
+
+#Creating table as per requirement
+# sql = """ CREATE TABLE IF NOT EXISTS test (
+#                                     name TEXT,
+#                                     link TEXT,
+#                                     ingredients TEXT,
+#                                 ); """
+cursor.execute(""" CREATE TABLE IF NOT EXISTS test (name TEXT, link TEXT, ingredients TEXT); """)
+conn.commit()
+
+
+ingredients = ['2 tablespoons peanut oil, divided', '1  shallot, finely chopped', '¼  white onion, chopped', '2 tablespoons butter', '1 tablespoon ginger garlic paste', '2 teaspoons lemon juice', '2 teaspoons garam masala, divided', '1 teaspoon chili powder', '1 teaspoon ground cumin']
+ingredient_str = ''
+for x in ingredients:
+    ingredient_str += ' ' + x
+
+cursor.execute('INSERT INTO test VALUES ("{Chicken Makhani Indian Butter Chicken}", "{https://www.allrecipes.com/recipe/45957/chicken-makhani-indian-butter-chicken/}", "{ingredient_str}");')
+
+conn.close()
