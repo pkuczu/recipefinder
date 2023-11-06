e5cfe9845107f373b5dd44ecd5c2fd2d142275a0
Vivian Chen <vivian3@illinois.edu>
2023-11-06 20:00:06 +0000

    Finished search function

--- webscraping_folder/recipes_search.py
+++ webscraping_folder/recipes_search.py
@@ -1,27 +1,65 @@
 import sqlite3
 from sqlite3 import Error
 
-def search_ingredient(ingredient):
+def search_ingredient(ingredient):      # returns a list of the recipes that have the ingredient
     conn = sqlite3.connect('recipes_test.db')
     cursor = conn.cursor()
 
+    recipes_containing_ingredient = []
+
+    cursor.execute('''SELECT name FROM test''')
+    all_names = cursor.fetchall()
+    # print(all_names)
+
     cursor.execute('''SELECT ingredients FROM test''')
-    ingredients = cursor.fetchall()      # a string of all the ingredients together
-    print(ingredient)
+    all_ingredients = cursor.fetchall()      # a string of all the ingredients together, comma separated
+    # print(all_ingredients)
+
+    ingredient = ingredient.lower()
+
+    for i in range(len(all_names)):
+        recipe_name = all_names[i]
+        recipe_name = recipe_name[0]
+        recipe_name = recipe_name.replace('{', '').replace('}', '')
+
+        ingredients_list = all_ingredients[i]
+
+        ingredients_list = ingredients_list[0]
+        ingredients_list = ingredients_list.replace('{', '').replace('}', '')
+        ingredients_list = ingredients_list.split(", ")
+
+        for item in ingredients_list:
+            if item == ingredient:
+                recipes_containing_ingredient.append(recipe_name)
+    
+    # for ingredients_list in all_ingredients:
+        # print(ingredients_list)
+        # ingredients_listed = ingredients_list[0]
+        # ingredients_listed = ingredients_listed.split(", ")
+        
+        # for item in ingredients_listed:
+            # if (item == ingredient):
+                # print(f"The ingredient {ingredient} was found in the recipe ")
+
+    # print(ingredients)
 
     conn.close()
 
-conn = sqlite3.connect('recipes_test.db')
-cursor = conn.cursor()
-table_name = 'test'
+    return recipes_containing_ingredient
 
-cursor.execute(f"PRAGMA table_info({table_name})")
+print(search_ingredient("ingredient_str"))
+
+# conn = sqlite3.connect('recipes_test.db')
+# cursor = conn.cursor()
+# table_name = 'test'
+
+# cursor.execute(f"PRAGMA table_info({table_name})")
 
 # Fetch and print the column names
-column_names = [row[1] for row in cursor.fetchall()]
-print("Column names:")
-for name in column_names:
-    print(name)
+# column_names = [row[1] for row in cursor.fetchall()]
+# print("Column names:")
+# for name in column_names:
+    # print(name)
 
 
 # cursor.execute("SELECT link, ingredient FROM test")
