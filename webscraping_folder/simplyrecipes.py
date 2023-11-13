72901b72aafaf3600b2402ba9ebb9cd11b5366e6
Sarah Dowden <111834004+sdowdenillini@users.noreply.github.com>
2023-11-13 20:50:24 +0000

    Updated Simply Recipes to pull all pages

--- webscraping_folder/simplyrecipes.py
+++ webscraping_folder/simplyrecipes.py
@@ -2,9 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import sqlite3
 
-conn = sqlite3.connect('recipes.sql')
+# branch name: updatingsimplyrecipes
 
-# current branch name: simplyRecipes
+conn = sqlite3.connect('recipes.sql')
 
 def getRecipeSimplyRecipes(ingredient):
     # Get first page 
@@ -17,7 +17,7 @@ def getRecipeSimplyRecipes(ingredient):
 
     num_repeat = len(page_html.find_all("a", class_="pagination__item")) + 1
 
-    for i in range(num_repeat):
+    for i in range(5):
         x = i * 24
         url = "https://www.simplyrecipes.com/search?q=" + ingredient + "&offset=" + str(x)
         if request.status_code != 200:
@@ -32,22 +32,22 @@ def getRecipeSimplyRecipes(ingredient):
             if (link.get("href").find("https://www.simplyrecipes.com/") != -1 and link.get("href").find(ingredient) != -1):
                 f.write(str(link.get("href")))
                 f.write('\n')
-            #getIngredientsAllRecipes(link.get("href"))
+            getIngredientsSimplyRecipes(link.get("href"))
         f.close()
 
-# def getIngredientsSimplyRecipes(url):
-#     request = requests.get(url)
-#     if request.status_code != 200:
-#         print("Error in retrieving the webpage.")
-#         exit()
-#     page_html = BeautifulSoup(request.content, 'html5lib')
+def getIngredientsSimplyRecipes(url):
+    request = requests.get(url)
+    if request.status_code != 200:
+        print("Error in retrieving the webpage.")
+        exit()
+    page_html = BeautifulSoup(request.content, 'html5lib')
 
-#     ingredients = page_html.find_all(class_="mntl-structured-ingredients__list-item")
-#     f2 = open("test2.txt", "a")
-#     for i in ingredients:
-#         ingredient = str(i.getText())
-#         f2.write(ingredient)
-#     f2.write('\n')
-#     f2.close
+    ingredients = page_html.find_all(class_="structured-ingredients__list-item")
+    f2 = open("test2.txt", "a")
+    for i in ingredients:
+        ingredient = str(i.getText())
+        f2.write(ingredient)
+    f2.write('\n')
+    f2.close
 
-getRecipeSimplyRecipes('asian')
+getRecipeSimplyRecipes('chicken')
