fba0d20b70eba8131bd53a5480100cbf404e3d80
Sarah Dowden <111834004+sdowdenillini@users.noreply.github.com>
2023-11-27 20:54:23 +0000

    creating actual database, not run yet

--- webscraping_folder/allRecipes.py
+++ webscraping_folder/allRecipes.py
@@ -60,21 +60,46 @@ def getIngredientsAllRecipes(url):
         ingredient_list.append(i.getText())
     return ingredient_list
 
-conn = sqlite3.connect('recipes_test.db')
-cursor = conn.cursor()
-# cursor.execute("DROP TABLE IF EXISTS test")
+def load_into_database(food):
+    conn = sqlite3.connect('actual_recipes.db')
+    cursor = conn.cursor()
+    # cursor.execute("DROP TABLE IF EXISTS test")
 
-cursor.execute(""" CREATE TABLE IF NOT EXISTS test (link TEXT, ingredients TEXT); """)
-conn.commit()
+    cursor.execute(""" CREATE TABLE IF NOT EXISTS test (link TEXT, ingredients TEXT); """)
+    conn.commit()
 
-getRecipeAllRecipes('squid')
+    getRecipeAllRecipes('squid')
 
-conn.commit()
+    conn.commit()
 
-# cursor.execute('''INSERT INTO test VALUES (?, ?)''', ('actual_link', 'ingredient_str'))
+    # cursor.execute('''INSERT INTO test VALUES (?, ?)''', ('actual_link', 'ingredient_str'))
 
-cursor.execute('''SELECT * FROM test''')
-results = cursor.fetchall()
-print(results)
+    cursor.execute('''SELECT * FROM test''')
+    results = cursor.fetchall()
+    print(results)
 
-conn.close()
+    conn.close()
+
+# List of things to input into the database
+meat_list = ['chicken', 'beef', 'steak', 'turkey', 'lamb', 'veal', 'pork', 'sausage', 'duck', 'goose', 'octopus', 'fish', 'salmon', 'tilapia', 'crab', 'lobsters', 'mussels', 'shrimp', 'prawns', 'oysters', 'scallops', 'clams']
+vegetable_list = ['broccoli', 'carrots', 'asparagus', 'green beans', 'tomatoes', 'bok choy', 'broccolini', 'celery', 'corn', 'snap peas', 'peas', 'peppers', 'squash', 'brussel sprouts', 'cabbage', 'beet', 'cauliflower', 'chicory', 'cucumbers', 'zucchini', 'radish', 'lettuce', 'spinach', 'mushrooms', 'tomatillo']
+fruit_list = ['mango', 'apple', 'orange', 'banana', 'grape', 'kiwi', 'cherry', 'avocado', 'grapefruit', 'pineapple', 'cranberry', 'berries', 'blueberries', 'strawberries', 'blackberries', 'apricot', 'peach', 'lemon', 'lime', 'pear', 'guava', 'plum', 'raspberry', 'pomegranate', 'watermelon']
+spices_list = ['cinnamon', 'oregano', 'italian seasoning', 'basil', 'herbes de provence', 'chives', 'cilantro', 'dill', 'lemongrass', 'marjoram', 'green onion', 'parsley', 'garlic', 'onions', 'rosemary', 'sage', 'tarragon', 'thyme', 'mint', 'pepper', 'salt', 'clove', 'fenugreek', 'cardamom', 'allspice', 'paprika', 'mustard', 'caraway', 'star anise', 'fennel', 'ancho', 'red pepper flakes', 'coriander', 'mace', 'sumac', 'cumin', 'chili powder', 'tumeric', 'garam masala', 'nutmeg', 'cayenne', 'zest', 'chocolate']
+types_of_cooking_styles_list = ['spatchcock', 'braising', 'stewing', 'pot roasting', 'barbecuing', 'smoking', 'pan sear', 'grilling', 'broiling', 'oven roast', 'stir frying', 'deep frying', 'shallow fry', 'fry', 'roast', 'bake']
+
+
+
+for food in meat_list:
+    load_into_database(food)
+
+for food in vegetable_list:
+    load_into_database(food)
+
+for food in fruit_list:
+    load_into_database(food)
+
+for food in spices_list:
+    load_into_database(food)
+
+for food in types_of_cooking_styles_list:
+    load_into_database(food)
